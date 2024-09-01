44fc6cd396f6431f0308c3b7fba06d20f9e3702b
davedude1011 <davedude.productions.inc@gmail.com>
2024-09-01 17:03:14 +0100

    edits

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -7,7 +7,6 @@ import * as schema from "./schema";
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
- */
 const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
@@ -16,3 +15,4 @@ const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
+ */
